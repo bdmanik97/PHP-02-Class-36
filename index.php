5fbab4f3f3e53746126af2ab93bdba34720fcde7
bdmanik97 <inmanik97@gmail.com>
2024-02-06 14:55:54 +0600

    php final

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
 
-$x = 40;
+// $x = 20;
 
-if($x == 10){
-    echo 'this is= 10';
+// if($x == 20){
+//     echo 'this is= 20';
+// }
+// else{
+//     echo 'this value is not =10';
+// }
+
+$x = 100;
+
+switch($x){
+    case 10;
+    echo'This value is 10';
+    break;
+
+    case 20;
+    echo'This value is 20';
+    break;
+
+    case 100;
+    echo'This value is 100';
+    break;
+
+    default:
+    echo 'None of the value is existing here';
+    break;
 }
-else{
-    echo 'this value is not =10';
-}
-
-
 
 
 ?>
